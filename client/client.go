200d45e3e8b71fe4a40e2889d17ea51c1e5f646c
tawarme <tawarme@example.com>
2021-12-28 19:32:32 -0500

    Added argument handling to client

--- client/client.go
+++ client/client.go
@@ -4,12 +4,36 @@ package main
 import (
 	"fmt"
 	"example.com/filesrv/client/clientlib"
+	"flag"
 )
 
 
 func main() {
 	server_add := "localhost"
 
+
+	channel := flag.Int("channel", 1, "Channel to send file.")
+
+	flag.Parse()
+
+	mode := flag.Arg(0)
+
+	file_path := ""
+	switch mode {
+	case "send":
+		file_path = flag.Arg(1)
+		if file_path == "" {
+			fmt.Println("Missing file path")
+			return
+		}
+		break
+	case "receive":
+		break
+	default:
+		fmt.Println("Invalid mode:", mode)
+		return
+	}
+
 	fmt.Println("Client started. Server is:", server_add)
-	clientlib.Client(server_add)
+	clientlib.Client(server_add, mode, *channel, file_path)
 }
