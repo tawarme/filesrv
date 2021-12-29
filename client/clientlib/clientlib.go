f4a475062960f19eacd4d52863137605c7aa03a2
tawarme <tawarme@example.com>
2021-12-28 19:33:40 -0500

    Added basic connection to server

--- client/clientlib/clientlib.go
+++ client/clientlib/clientlib.go
@@ -7,6 +7,13 @@ import (
 )
 
 
-func Client(server_add string) {
+func Client(server_add string, mode string, channel int, file_path string) {
+	server, err := net.Dial("tcp", server_add+":5000")
 
+	if err != nil {
+		fmt.Println(err)
+		return 
+	}
+
+	fmt.Println("Connected to server", server)
 }
