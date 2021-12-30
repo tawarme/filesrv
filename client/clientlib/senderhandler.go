72da0680c5998370a65fdfe6b16304ae069ca682
tawarme <tawarme@example.com>
2021-12-29 19:01:41 -0500

    Removed debug prints

--- client/clientlib/senderhandler.go
+++ client/clientlib/senderhandler.go
@@ -13,16 +13,16 @@ import (
 func SenderHandler(server net.Conn, channel int, file_path string) {
 	fmt.Println("Sending to channel", channel, "file", file_path)
 
-	dir, file_name := filepath.Split(file_path)
-	fmt.Println(dir, file_name)
+	_, file_name := filepath.Split(file_path)
+
 
 	channel_enc := make([]byte, 4)
 	binary.BigEndian.PutUint32(channel_enc, uint32(channel))
 
-	fmt.Println(channel_enc)
 
 	name_length := make([]byte, 1)
 	name_length[0] = uint8(len(file_name))
+
 
 	f, err := os.Open(file_path)
 
