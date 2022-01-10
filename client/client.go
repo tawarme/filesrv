bae35e61941179b42a91b96eddedaef58cfc5463
tawarme <tawarme@example.com>
2022-01-10 17:20:23 -0500

    Fixed bug: Channel flag ignored in client CLI.

--- client/client.go
+++ client/client.go
@@ -5,30 +5,48 @@ import (
 	"fmt"
 	"example.com/filesrv/client/clientlib"
 	"flag"
+	"os"
 )
 
 
 func main() {
 	server_add := "localhost"
 
+	sendCmd := flag.NewFlagSet("send", flag.ExitOnError)
+	sendChannel := sendCmd.Int("channel", 1, "Channel to send file.")
 
-	channel := flag.Int("channel", 1, "Channel to send file.")
+	receiveCmd := flag.NewFlagSet("send", flag.ExitOnError)
+	receiveChannel := receiveCmd.Int("channel", 1, "Channel to send file.")
 
-	flag.Parse()
+	if len(os.Args) < 2 {
+		fmt.Println("Missing mode")
+		fmt.Println("Valid modes: send receive")
+		return
+	}
+	mode := os.Args[1]
 
-	mode := flag.Arg(0)
-
+	var channel *int
 	file_path := ""
+
 	switch mode {
 	case "send":
-		file_path = flag.Arg(1)
-		if file_path == "" {
+		if len(os.Args) < 3 {
 			fmt.Println("Missing file path")
 			return
 		}
+
+		file_path = os.Args[2]
+
+		sendCmd.Parse(os.Args[3:])
+		channel = sendChannel
+
 		break
+
 	case "receive":
+		receiveCmd.Parse(os.Args[2:])
+		channel = receiveChannel
 		break
+
 	default:
 		fmt.Println("Invalid mode:", mode)
 		fmt.Println("Valid modes: send receive")
